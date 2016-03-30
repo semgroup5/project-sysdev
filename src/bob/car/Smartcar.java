ecc73d405d90a3dc61ecb3b17c343746d82385e0
Jonatan <jonatan.petur@gmail.com>
2016-03-30 14:48:01 +0200

    Did stuff, but more

--- src/bob/car/Smartcar.java
+++ src/bob/car/Smartcar.java
@@ -9,18 +9,28 @@ import java.net.Socket;
  */
 public class Smartcar implements ISmartcar{
     Socket socket;
-
+    public final String ip;
+    public final int port;
+    Exception connectionException;
     public Smartcar(String ip, int port) {
         //Start thread to initialize connection
-        //Thread Start
+        this.ip = ip;
+        this.port = port;
 
-
-
+        //Thread START
+        SmartcarConnectionMaker connectionMaker = new SmartcarConnectionMaker(this);
+        Thread ConnectionThread = new Thread(connectionMaker);
+        ConnectionThread.run();
         //Thread END
     }
 
     public void setConnection(Socket socket){
         this.socket = socket;
+    }
+
+    public void setConnectionException(Exception e)
+    {
+        this.connectionException = e;
     }
 
     @Override
@@ -35,21 +45,6 @@ public class Smartcar implements ISmartcar{
 
     @Override
     public void setSpeed(int speed) throws Exception {
-
-    }
-}
-
-public class ConnectionMaker implements Runnable{
-    String ip;
-    int port;
-
-    public ConnectionMaker(String ip, int port) {
-        this.ip = ip;
-        this.port = port;
-    }
-
-    @Override
-    public void run() {
 
     }
 }
