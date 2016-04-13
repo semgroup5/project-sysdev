0d063c4aeb3330c8087253e82e104c491219d07a
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2016-04-13 23:57:55 +0200

    Project update

--- modules/car/src/sem/group5/bob/car/SmartcarComm.java
+++ modules/car/src/sem/group5/bob/car/SmartcarComm.java
@@ -1,18 +1,33 @@
 package sem.group5.bob.car;
 
+import sem.group5.bob.car.streaming.SerialPortConnect;
+
 import java.io.BufferedReader;
 import java.io.OutputStream;
 
-public class SmartcarComm {
+public class SmartCarComm {
     public BufferedReader input;
     public OutputStream output;
+    SerialConnect obj;
+    SerialPortConnect con;
 
-    public SmartcarComm() throws NoSuchFieldError{
-        SerialConnect obj = new SerialConnect();
+    public SmartCarComm() throws NoSuchFieldError{
+        obj = new SerialConnect();
         obj.initialize();
         input = SerialConnect.input;
         output = SerialConnect.output;
+//        con = new SerialPortConnect();
+//        con.initialize();
     }
+
+//    public synchronized void writeData(String data) {
+//        try {
+//            System.out.println("Sending : " + data);
+//            con.write(data);
+//        } catch (Exception e) {
+//            System.out.println("could not write to port");
+//        }
+//    }
 
     public synchronized void writeData(String data) {
         try {
@@ -40,5 +55,12 @@ public class SmartcarComm {
         if(angle < 360 && angle > -360){
             writeData("r" + angle + "/");
         }
+    }
+
+    /**
+     * here
+     */
+    public void closeConnection() {
+        obj.close();
     }
 }
