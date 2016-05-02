657534ee9b53572a6b710c953e34624605cb2219
Jonatan <jonatan.petur@gmail.com>
2016-05-02 11:00:23 +0200

    style fix

--- modules/client/src/sem/group5/bob/client/ScanLineGenerator.java
+++ modules/client/src/sem/group5/bob/client/ScanLineGenerator.java
@@ -5,17 +5,18 @@ import java.util.Observable;
 import java.util.Observer;
 
 class ScanLineGenerator extends Observable implements Observer {
-    private static int[] GenerateLine(BufferedImage image){
+    private static int[] generateLine(BufferedImage image){
         int[] distanceArray = new int[640];
         for(int i = 0; i < 640; i++){
             distanceArray[i]=image.getRGB(240, i);
         }
-        return  distanceArray;
+        return distanceArray;
     }
 
     @Override
     public void update(Observable observable, Object o) {
         BufferedImage image = (BufferedImage) o;
-        this.notifyObservers(GenerateLine(image));
+        setChanged();
+        notifyObservers(generateLine(image));
     }
 }
