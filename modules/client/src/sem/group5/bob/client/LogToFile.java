29bade1ff58fd7db98ef0947a15460a4b5dd99c5
CYuZeric <chyj1993@gmail.com>
2016-05-17 06:41:30 +0200

    recreation of LogToFile class. also added Cross-platform support.

--- modules/client/src/sem/group5/bob/client/LogToFile.java
+++ modules/client/src/sem/group5/bob/client/LogToFile.java
@@ -2,63 +2,91 @@ package sem.group5.bob.client;
 
 
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
-
 /**
- * Created by chyj1 on 2016-5-12.
+ * Created by GeoffreyC on 2016-5-12.
  */
 
 public class LogToFile {
-    String time;
-    String fileName;
-    String fileLocation;
-    PrintWriter writer;
+
+    private String fileLocation;
+    private String OS = System.getProperty("os.name", "").toUpperCase();
+
+    ArrayList<String> logData = new ArrayList<String>() ;
 
     public static void main(String arg[]) throws IOException {
-
         LogToFile log = new LogToFile();
-        log.LogToFile();
+        log.crtDirl();
+
         for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-            log.writer("hahahha");
-        }
-        log.close();
+            int j = i+1;
+            log.addToList("This is the "+j+"time(s) it writes now...");
+      }
+      log.logWriter();
     }
 
-    public void LogToFile() {
-        try {
-            fileLocation = fileName();
-            writer = new PrintWriter(fileLocation, "UTF-8");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
+    public void addToList(String string){
+        this.logData.add(string);
     }
 
-    public void writer(String string) {
-        try {
-
-            writer.println(string);
-
-        } catch (Exception e) {
-
-        }
+    public void resetList(){
+        this.logData.clear();
     }
 
-    public void close() {
+
+    public void logWriter() {
+        crtFile(OS);
+        try{
+        PrintWriter writer = new PrintWriter(fileLocation, "UTF-8");
+        for (int i = 0; i < logData.size(); i++) {
+            writer.println(logData.get(i));
+        }
         writer.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
-    public String fileName() {
+    public void crtDirl() {
+        if (OS.startsWith("WINDOWS")) {
+            File createDir1 = new File(System.getenv("HOMEDRIVE") + System.getenv("HOMEPATH")
+                    + File.separator +"BobCar");
+            createDir1.mkdirs();
+        } else if (OS.startsWith("LINUX")) {
+            File createDir1 = new File("/home/BobCar");
+            createDir1.mkdirs();
+        } else if (OS.startsWith("MAC")) {
+            File createDir1 = new File(System.getProperty("user.home")+File.separator+"Documents" +File.separator+"BobCar");
+            createDir1.mkdirs();
+        } else {
+            System.out.println("Sorry, your operating system is different");
+        }
+    }
+
+    public void crtFile(String OS){
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH：mm：ss@yyyy-MM-dd");
-        time = sdf.format(cal.getTime());
-        fileName = "BobCarLog" + time + ".txt";
-        fileLocation = System.getenv("APPDATA") + "\\BobCar" + "\\" + fileName;
-        return fileLocation;
+        String time = sdf.format(cal.getTime());
+        String fileName = "BobCarLog" + time + ".txt";
+        this.fileLocation = "~/Documents/BobCar/"+ fileName;
+        if(OS.startsWith("WINDOWS")) {
+            this.fileLocation = System.getenv("HOMEDRIVE") + System.getenv("HOMEPATH")
+                    + File.separator +"BobCar" + File.separator + fileName;
+        }
+        else if(OS.startsWith("LINUX")){
+            this.fileLocation = "/home/BobCar/"+ fileName;
+        }
+        else if(OS.startsWith("MAC")){
+            this.fileLocation = System.getProperty("user.home")+File.separator+"Documents"
+                    + File.separator +"BobCar"+ File.separator+ fileName;
+        }
     }
 }
