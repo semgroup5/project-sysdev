5de629abe91252005344acbeb0236fdc1c1395b9
Geoffrey <chyj1993@gmail.com>
2016-05-23 11:05:51 +0200

    ...

--- modules/client/src/sem/group5/bob/client/LogToFile.java
+++ modules/client/src/sem/group5/bob/client/LogToFile.java
@@ -22,7 +22,7 @@ public class LogToFile implements Observer{
 
         for (int i = 0; i < 5; i++) {
             int j = i+1;
-            log.addToList("This is the "+j+"time(s) it writes to the file now...");
+            log.addToList("This is the "+j+"time(s) it writes to the log now...");
         }
         log.logWriter();
     }
