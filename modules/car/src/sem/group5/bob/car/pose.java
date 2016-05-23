cd058575daf1e2ba30c8b9ef641b77985acea4db
Raphael Puccinelli <raphaelpuccinelli@gmail.com>
2016-05-23 11:51:05 +0200

    Fixing

--- modules/car/src/sem/group5/bob/car/pose.java
+++ modules/car/src/sem/group5/bob/car/pose.java
@@ -4,8 +4,10 @@ import java.util.Observable;
 import java.util.Observer;
 
 /**
- * todo
+ * Created by GeoffreyC on 2016/5/1.
+ * Updated by Geoffrey, Axel and Emanuel at 16-05-06
  */
+
 public class Pose extends SerialConnect implements Observer {
 
 
@@ -15,11 +17,11 @@ public class Pose extends SerialConnect implements Observer {
     /**
      * Round up the number the digits can be selected.
      *
-     * @param a todo
-     * @param r todo
-     * @return todo
+     * @param a
+     * @param r
+     * @return
      */
-    private static double rdNum(Double a, int r) {
+    public static double rdNum(Double a, int r) {
         if (r < 0) throw new IllegalArgumentException();
 
         long factor = (long) Math.pow(10, r);
@@ -32,15 +34,15 @@ public class Pose extends SerialConnect implements Observer {
      * Breaks down the raw data from the arduino to values
      * @param locationData String that holds the raw data
      */
-    private void breakDown(String locationData) {
+    public void breakDown(String locationData) {
         this.angle = Double.parseDouble(locationData.substring(locationData.indexOf("a") + 1, locationData.indexOf("d")));
         this.disp = Double.parseDouble(locationData.substring(locationData.indexOf("d") + 1, locationData.indexOf("/")));
     }
 
     /**
-     *todo
+     *
      */
-    private void calculatePose() {
+    public void calculatePose() {
 
         double dispOld = 0, x, y;
         double dispTmp = disp - dispOld;
@@ -68,16 +70,11 @@ public class Pose extends SerialConnect implements Observer {
             this.X += rdNum(x, 3);
             this.Y += rdNum(y, 3);
             dispOld += dispTmp;
-            System.out.println(X + " this is the X ");
-            System.out.println(Y + " this is the Y ");
+            System.out.println(X + "this is the X");
+            System.out.println(Y + "this is the Y");
         }
     }
 
-    /**
-     * todo
-     * @param o todo
-     * @param arg todo
-     */
     public void update(Observable o, Object arg) {
         String locationData = (String) arg;
         breakDown(locationData);
@@ -88,7 +85,7 @@ public class Pose extends SerialConnect implements Observer {
      */
     public String getLatestPose() {
         calculatePose();
-        System.out.print(" This is the data sent from getLatestPose: " + " X" + X + " Y" + Y + " Ang" + angle);
+        System.out.print("This is the data sent from getLatestPose: " + "X" + X + "Y" + Y + "Ang" + angle);
         return "X" + X + "Y" + Y + "Ang" + angle;
     }
 }
